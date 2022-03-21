54519d84952c7818dd488f2852eb42b6fe866f6c
Mauricio Campos <thoyferr@gmail.com>
2022-03-21 17:43:49 -0300

    finalizado

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,23 +1,42 @@
 
 const baseURL = 'https://jsonplaceholder.typicode.com/';
 
-
-
 const obtenerDatos = async () => {
   const arregloDatos = [];
-  for (let i = 1; i < 21; i++) {
+  i=1;
+  do{
     const datos = await fetch(`${baseURL}photos?id=${i}`);
-    const json =  await datos.json();
-    arregloDatos.push(json);
-  }
-  return arregloDatos
+    try{
+      const json =  await datos.json();
+      arregloDatos.push(json);
+      i = i+1;
+    }catch(error){
+      console.log('error al encontrar promesa');
+    }
+  }while(i<21);
+    return arregloDatos
 }
-
 
 obtenerDatos()
  .then((datos) => {
   datos.forEach( dato => console.log(`El Titulo del Id ${dato[0].id} es: ${dato[0].title}`) );
+  mensaje(datos);
 });
+
+
+ const mensaje = (dato) => {
+  const inforacion = console.log('Información Enviada');
+  return new Promise((resolve) => {
+      setTimeout(() => {
+          if(dato){
+              resolve(inforacion);
+          }else{
+            reject(`Sin informacion`);
+        }
+      },3000); 
+  });
+}
+
 
 
 
